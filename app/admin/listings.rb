5322a1ce449d341e28f747a5d9b6c7c95c6d9331
Raghu Betina <raghu@firstdraft.com>
2018-03-05 04:18:00 +0000

    Whitelist columns in Listing for ActiveAdmin

--- app/admin/listings.rb
+++ app/admin/listings.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Listing do
+
+ permit_params :type_id, :category_id, :name, :description, :price, :user_id, :images, :purchase_date, :link, :status
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
